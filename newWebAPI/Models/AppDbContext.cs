e177deb3a0d61d03af3a298a44759a69c23323d9
DarmonNoah <noah.darmon@ecole-isitech.fr>
2023-12-05 14:00:17 +0100

    réponses proposées pour Cours & modif Appdb

--- newWebAPI/Models/AppDbContext.cs
+++ newWebAPI/Models/AppDbContext.cs
@@ -5,13 +5,42 @@ namespace newWebAPI.Models;
 
 public class AppDbContext : DbContext
 {
-    private const string ConnectionString=@"Server=localhost;Database=BookDb;Trusted_Connection=True;";
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        optionBuilder.UseSqlite(ConnectionString);
-    }
 
-    public DbSet<Book> Books {get;set;}
+    }
+    //private const string ConnectionString=@"Server=localhost;Database=BookDb;Trusted_Connection=True;";
+    private const string ConnectionString="Data Source = Books.db;";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var currentDir = Directory. GetCurrentDirectory();
+        var dbPath = Path.Combine(currentDir, "Books.db");
+        optionsBuilder.UseSqlite($"Filename={dbPath}");
+    }
+       /*
+     new Book
+    {
+        Id = 2,
+        Title = "Professional C# 7 and .NET Core 2.0",
+        Author = "Christian Nagel",
+        Description = "A true masterclass in C# and .NET programming",
+        Genre = "Software",
+        Price = 50,
+        PublishDate = new DateTime(2018, 04, 18)
+    },
+
+        new Book
+        {
+        Id = 3,
+        Title = "Professional C# 8 and .NET Core 3.0",
+        Author = "Christian Nagel",
+        Description = "A true masterclass in C# and .NET programming",
+        Genre = "Software",
+        Price = 50,
+        PublishDate = new DateTime(2019, 10, 30)
+        }
+*/
+    public DbSet<Book> Books {get;set;} = default!;
     
 }
